4c079d9398efe74983dc4586edf4313621059457
NK-codeman0001 <neerajkumarsingh1999@gmail.com>
2021-08-02 13:11:49 +0530

    third commit by neeraj

--- app.js
+++ app.js
@@ -57,6 +57,7 @@ if ( process.env.NODE_ENV == "production"){
   })
 }
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log("Backend server is running!");
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
