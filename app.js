cdbab4cdfe7437e25e478174e88ee8f56f6afed5
NK-codeman0001 <neerajkumarsingh1999@gmail.com>
2021-08-02 12:56:40 +0530

    second commit by neeraj

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const router = express.Router();
 const path = require("path");
 
 dotenv.config();
-const PORT = process.env.PORT || 5000 ;
 mongoose.connect(
   process.env.MONGO_URL,
   { useNewUrlParser: true, useUnifiedTopology: true},
@@ -52,12 +51,12 @@ app.use("/api/posts", postRoute);
 // for heroku
 if ( process.env.NODE_ENV == "production"){
   app.use(express.static("client/build"));
-  // const path = require("path");
-  // app.get("*", (req, res) => {
-  //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  // })
+  const path = require("path");
+  app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
 }
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
 });
